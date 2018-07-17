a937c6d76e77b9f6a3e0d3db79408b59342e1469
Joao Figueiredo <joao.figueiredo@jfig.net>
2018-07-17 11:13:13 +0100

    adds footer

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -19,45 +19,32 @@ class Footer extends React.Component {
     return (
 
 <footer>
-  <div /* Back Home */ >
-    <h3>
-    <Link
-      to={'/'}
-    >
-      {siteTitle}
-    </Link>
-    </h3>
-  </div>
+
   <div /* LCD Links */
-    className={styles.centerWrapper}
+    className={styles.lcdlinks}
+    style={{display: 'flex', justifyContent: 'center'}}
   >
-    <div className={styles.lcdlinks}>
-      <li><a href='https://lcdporto.org'>LCD Porto<br />(website)</a></li>
-      <li><a href='https://github.com/lcdporto/'><img src={gh} /></a></li>
-      <li><a href='https://facebook.com/lcdporto/'><img src={fb} /></a></li>
-      <li><a href='https://www.instagram.com/lcdporto/'><img src={ig} /></a></li>
-      <li><a href='https://twitter.com/lcdporto/'><img src={tw} /></a></li>
-    </div>
-  </div>
+    <li><a href='https://lcdporto.org' title='Website'><b>LCD Porto</b></a></li>
+    <li><a href='https://github.com/lcdporto/'><img src={gh} /></a></li>
+    <li><a href='https://facebook.com/lcdporto/'><img src={fb} /></a></li>
+    <li><a href='https://www.instagram.com/lcdporto/'><img src={ig} /></a></li>
+    <li><a href='https://twitter.com/lcdporto/'><img src={tw} /></a></li>
+  </div /* /LCD Links */>
 
 
   <div /* cc license */
     className={styles.cc}
+    style={{display: 'flex', justifyContent: 'center'}}
   >
-    <div className={styles.ccIMG} >
-      <a href="https://creativecommons.org/licenses/by/4.0/legalcode.pt">
-        <img src={cc} />
-        <img src={by} />
-      </a>
-    </div /* .ccIMG */>
-    <div>
-      A não ser que explicitamente indicado o <b>{siteTitle}</b> <br />
-      está disponível sob a licensa
-      <a href="https://creativecommons.org/licenses/by/4.0/legalcode.pt">
-        Atribuição 4.0 Internacional
-      </a>
-    </div>
-  </div>
+    <a href="https://creativecommons.org/licenses/by/4.0/legalcode.pt">
+      <img src={cc} />
+      <img src={by} />
+    </a>
+    <a href="https://creativecommons.org/licenses/by/4.0/legalcode.pt">
+      A não ser que explicitamente indicado o <b>{siteTitle}</b> está
+      disponível sob a licensa Atribuição 4.0 Internacional
+    </a>
+  </div /* /cc license */ >
 
 
 </footer>
