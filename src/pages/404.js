6db68a91bab1d7b16cc4e9354205a5cf57a9f268
Joao Figueiredo <joao.figueiredo@jfig.net>
2018-07-17 22:47:40 +0100

    changes 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import img404 from '../assets/404.jpg'
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>WTF 404!</h1>
+    <p>A página não existe. É melhor ir até à <Link to={'/'}>Homepage</Link>.</p>
+    <img src={img404} />
   </div>
 )
 
